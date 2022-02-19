30acd5696972492b1a3a078a5f51fed3dcc01eea
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-02-19 23:24:12 +0100

    Add comment controller and routes && create / retrieve

---
+++ backend/routes/comment.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Import du controller
+const commentCtrl = require('../controllers/comment');
+
+// Import des middlewares
+const auth = require('../middlewares/auth');
+
+// GET 
+router.get('/test', commentCtrl.test);
+router.get('/retrieve/:identerprise/:idproject', auth, commentCtrl.getComment);
+
+// POST 
+router.post('/create/:idproject', auth, commentCtrl.createComment);
+
+module.exports = router;
